849369526c511a2950e057d57f77cc18a281d638
scurry222 <scurry222@gmail.com>
2021-01-04 11:57:53 -0800

    Add stalemate, reset functionality, styling

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 (function() {
     const $areas = document.querySelectorAll('.area');
-    const moves = {};
+    let moves = {
+        1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: ''
+    };
     let player = true;
+
+    const $winScreen = document.createElement('div')
+    $winScreen.classList.add('win-screen');
+
+    const $resetButton = document.createElement('button');
+    $resetButton.innerText = 'Play Again?';
+
+    $resetButton.addEventListener('click', () => {
+        moves = { 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: '' };
+
+        $areas.forEach(($area) => {
+            $area.innerText = '';
+        })
+        $winScreen.remove();
+        $resetButton.remove();
+    })
+
+    const win = function(token) {
+        return (
+            (moves[1] === token && moves[5] === token && moves[9] === token) ||
+            (moves[3] === token && moves[4] === token && moves[7] === token) ||
+            (moves[1] === token && moves[2] === token && moves[3] === token) ||
+            (moves[4] === token && moves[5] === token && moves[6] === token) ||
+            (moves[7] === token && moves[8] === token && moves[9] === token) ||
+            (moves[1] === token && moves[4] === token && moves[7] === token) ||
+            (moves[2] === token && moves[5] === token && moves[8] === token) ||
+            (moves[3] === token && moves[6] === token && moves[9] === token))
+        ? true: false;
+    }
+
+    const stalemate = function() {
+        return !Object.values(moves).includes('');
+    }
+
+    const renderReset = function(message) {
+        $winScreen.innerText = message;
+        document.body.appendChild($winScreen);
+        $winScreen.appendChild($resetButton);
+    }
 
     $areas.forEach(($area) => {
         $area.addEventListener('click', () => {
@@ -9,8 +50,10 @@
                 const token = player ? 'X' : 'O';
                 moves[$area.id] = token;
                 $area.innerText = token;
+                win(token) ? renderReset(`Player ${token} wins!`) :
+                stalemate() ? renderReset('Stalemate!'): null;
+                player = !player;
             }
-            player = !player;
         });
     });
 })()
